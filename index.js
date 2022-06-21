09598c559d543e24453a7f0f69247bd578cb525a
unknown <lau1087054@gmail.com>
2022-06-21 15:52:18 -0700

    questions added

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    "What did you learn about this assignment?",
+    "What did you struggle with this assignment?",
+    "How do you feel after this assignment?",
+    "Did this assignment go the way you imagined it to go?",
+    ];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
