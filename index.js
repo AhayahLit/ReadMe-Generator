044ca1e453b32f204cdb8af80a9deae9568930ae
unknown <lau1087054@gmail.com>
2022-06-21 18:48:08 -0700

    minor edits

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [{
         type: "input",
-        message: "What is the project name?"
+        message: "What is the project name?",
         name: "Title"
 }, {
     type: "input",
@@ -15,7 +16,7 @@ const questions = [{
     name: "Table of Contents"
 }, {
     type: "Installation",
-    message: "How and what to install program necessary to operate program."
+    message: "How and what to install program necessary to operate program.",
     name: "Installation"
 }, {
     type: "input",
@@ -40,15 +41,21 @@ const questions = [{
 }   ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
+function writeToFile(fileName, data) 
+function(err) {
+    console.log(fileName)
+    console.log(data)
+    if (err) {
+        return console.log(err)
+    } else {
+        console.log("success")
+    }
 }
-
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then(function(data) {
-        writeToFile("README.md", generatorMarkdown(data);)
+        writeToFile("README.md", generatorMarkdown(data));
     })
 }
 
