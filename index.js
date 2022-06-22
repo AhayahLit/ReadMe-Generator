96833c06ec3562d863d780389e8deee9dfdc5980
unknown <lau1087054@gmail.com>
2022-06-21 17:51:37 -0700

    inputs

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [
-    "What did you learn about this assignment?",
-    "What did you struggle with this assignment?",
-    "How do you feel after this assignment?",
-    "Did this assignment go the way you imagined it to go?",
+const questions = [{
+        type: "input",
+        message: "What is the project name?"
+        name: "Title"
+}, {
+    type: "input",
+    message:"What is this project about?",
+    name: "Description"
+}, {
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents"
+}, {
+    type: "Installation",
+    message: "How and what to install program necessary to operate program."
+    name: "Installation"
+}, {
+    type: "input",
+    message: "How to use this program.",
+    name: "Usage"
+}, {
+    type: "input",
+    message: "What licenses are being used.",
+    name: "Licenses"
+}, {
+    type: "input",
+    message: "Who Contributed to this project?",
+    name: "Contributors"
+}, {
+    type: "input",
+    message: "What commands will you be using?",
+    name: "Tests"
+}
+ 
     ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
